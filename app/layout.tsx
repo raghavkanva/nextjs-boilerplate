a2ef2408fbdfb8a3395c7975ac2a140ac4ac4223
RAGHAV KANVA <raghavkanva@gmail.com>
2026-07-27 18:43:49 +0530

    Optimize animations and simplify tracking scripts
    Added `will-change` hints and `animation-fill-mode: forwards` for key entrance animation classes in `app/globals.css` to improve rendering performance and reduce unnecessary GPU layer lifetime. In `app/layout.tsx`, removed Facebook Pixel and temporary direct GA4/Ads scripts, keeping GTM as the single analytics loader path and cleaning up script markup.

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,79 +55,30 @@ export default function RootLayout({
         className={`${sora.variable} ${inter.variable} ${plexMono.variable} font-body bg-bg text-text antialiased`}
       >
         <noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-NW77K92B"
-    height="0"
-    width="0"
-    style={{
-      display: "none",
-      visibility: "hidden",
-    }}
-  />
-</noscript>
-
-<Script id="gtm" strategy="afterInteractive">
-  {`
-    (function(w,d,s,l,i){
-      w[l]=w[l]||[];
-      w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});
-      var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),
-      dl=l!='dataLayer'?'&l='+l:'';
-      j.async=true;
-      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-      f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NW77K92B');
-  `}
-</Script>
-        {/* Facebook Pixel */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;
-            n.push=n;
-            n.loaded=!0;
-            n.version='2.0';
-            n.queue=[];
-            t=b.createElement(e);
-            t.async=!0;
-            t.src=v;
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s);
-            }(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-
-            fbq('init', '1385045223537164');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1385045223537164&ev=PageView&noscript=1"
-            alt=""
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NW77K92B"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
           />
         </noscript>
 
-        {/* TEMPORARY - Keep GA4 until GTM is verified */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-986DLXGDMD"
-          strategy="afterInteractive"
-        />
-
-        <Script id="ga4-init" strategy="afterInteractive">
+        <Script id="gtm" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-986DLXGDMD');
-            gtag('config', 'AW-18328196277');
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NW77K92B');
           `}
         </Script>
 
@@ -144,7 +95,6 @@ export default function RootLayout({
 
         {children}
       </body>
-
     </html>
   );
 }
